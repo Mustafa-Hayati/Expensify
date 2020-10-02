999535b1a785a71a0a6c8b97e81acc7ff39c192e
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2020-10-02 16:07:29 +0330

    organize redux.

--- src/index.js
+++ src/index.js
@@ -3,5 +3,33 @@ import "./base_styles/index.scss";
 import React from "react";
 import ReactDOM from "react-dom";
 import AppRouter from "./routers/AppRouter";
+import configureStore from "./store/configureStore";
+import { addExpense } from "./actions/expensesAction";
+import { setTextFilter } from "./actions/filtersActions";
+import getVisibleExpenses from "./selectors/expenses";
+
+const store = configureStore();
+
+store.dispatch(
+  addExpense({
+    description: "Water bill",
+    amount: 2000,
+    note: "fuck government",
+    createdAt: Date.now(),
+  })
+);
+store.dispatch(
+  addExpense({
+    description: "Gas bill",
+    amount: 3000,
+    note: "fuck government",
+    createdAt: Date.now(),
+  })
+);
+
+store.dispatch(setTextFilter("water"));
+const state = store.getState();
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+console.log(visibleExpenses);
 
 ReactDOM.render(<AppRouter />, document.getElementById("root"));
