2de9a5c486dcab98481aea89e5d53d110971b500
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2020-10-03 13:03:28 +0330

    Add the newly created expense to the expenses and show it in the dashboard.

--- src/components/CreateExpense/CreateExpense.js
+++ src/components/CreateExpense/CreateExpense.js
@@ -1,13 +1,19 @@
 import React from "react";
+import { connect } from "react-redux";
 import ExpenseForm from "../ExpenseForm/ExpenseForm";
+import { addExpense } from "../../actions/expensesAction";
 
-const CreateExpense = () => {
+const CreateExpense = ({ history, dispatch }) => {
+  const onExpenseCreate = expense => {
+    dispatch(addExpense(expense));
+    history.push("/");
+  };
   return (
     <div>
       <h1>Add Expense</h1>
-      <ExpenseForm />
+      <ExpenseForm onExpenseCreate={onExpenseCreate} />
     </div>
   );
 };
 
-export default CreateExpense;
+export default connect()(CreateExpense);
--- src/components/ExpenseForm/ExpenseForm.js
+++ src/components/ExpenseForm/ExpenseForm.js
@@ -4,31 +4,52 @@ import "react-dates/lib/css/_datepicker.css";
 import { SingleDatePicker } from "react-dates";
 import "react-dates/initialize";
 
-const ExpenseForm = () => {
+const ExpenseForm = ({ onExpenseCreate, history }) => {
   const [description, setDescription] = useState("");
-  const [amount, setAmount] = useState(0);
+  const [amount, setAmount] = useState("");
   const [note, setNote] = useState("");
   const [createdAt, setCreatedAt] = useState(moment());
   const [calendarFocused, setCalendarFocused] = useState(false);
+  const [error, setError] = useState("");
 
   // const onInputChange = e => {
   //   const name = e.target.name;
   //   eval(`set${name}(e.target.value)`);
   // };
 
+  const onFormSubmit = e => {
+    e.preventDefault();
+
+    if (!description || !amount) {
+      setError("Please provide a description and amount");
+    } else {
+      setError("");
+      onExpenseCreate({
+        description,
+        amount: parseFloat(amount, 10) * 100,
+        note,
+        createdAt: createdAt.valueOf(),
+      });
+    }
+  };
+
   const onDescriptionChange = e => {
     setDescription(e.target.value);
+    setError("");
   };
 
   const onAmountChange = e => {
     const amount = e.target.value;
-    if (amount.match(/^\d*(\.\d{0,2})?$/)) {
+    if (!amount || amount.match(/^\d{1,}(\.\d{0,2})?$/)) {
       setAmount(amount);
+      setError("");
     }
   };
 
-  const onDateChange = createAd => {
-    setCreatedAt(createAd);
+  const onDateChange = createdAt => {
+    if (createdAt) {
+      setCreatedAt(createdAt);
+    }
   };
 
   const onFocusChange = ({ focused }) => {
@@ -37,7 +58,8 @@ const ExpenseForm = () => {
 
   return (
     <div>
-      <form>
+      {error && <p>{error}</p>}
+      <form onSubmit={onFormSubmit}>
         <input
           onChange={onDescriptionChange}
           // onChange={onInputChange}
