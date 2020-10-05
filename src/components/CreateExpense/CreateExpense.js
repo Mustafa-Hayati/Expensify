f52c66c3019df3b07fdd065493d88292d472eb78
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2020-10-05 21:01:17 +0330

    Change CreateExpense Page to use mapDispatchToProps instead of dispatching the addExpense(expense) directly.

--- src/components/CreateExpense/CreateExpense.js
+++ src/components/CreateExpense/CreateExpense.js
@@ -3,18 +3,25 @@ import { connect } from "react-redux";
 import ExpenseForm from "../ExpenseForm/ExpenseForm";
 import { addExpense } from "../../actions/expensesAction";
 
-const CreateExpense = ({ history, dispatch }) => {
-  const onSubmit = expense => {
-    dispatch(addExpense(expense));
+const CreateExpense = ({ history, onSubmit }) => {
+  const onExpenseCreate = expense => {
+    // dispatch(addExpense(expense));
+    onSubmit(expense);
     history.push("/");
   };
 
   return (
     <div>
       <h2>Add Expense</h2>
-      <ExpenseForm onSubmit={onSubmit} />
+      <ExpenseForm onSubmit={onExpenseCreate} />
     </div>
   );
 };
 
-export default connect()(CreateExpense);
+const mapDispatchToProps = dispatch => {
+  return {
+    onSubmit: expense => dispatch(addExpense(expense)),
+  };
+};
+
+export default connect(undefined, mapDispatchToProps)(CreateExpense);
