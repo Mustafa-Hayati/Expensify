18648467cac5f6f605273a33d51355568950d63a
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2020-10-07 15:42:04 +0330

    Create the PrivateRotue component and change the create, edit and dashboard routes to be private.

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -2,12 +2,12 @@ import React, { Fragment } from "react";
 import { Router, Route, Switch } from "react-router-dom";
 import createHistory from "history/createBrowserHistory";
 import ExpenseDashboardPage from "../components/ExpenseDashboardPage/ExpenseDashboardPage";
-import Header from "../components/Header/Header";
 import CreateExpense from "./../components/CreateExpense/CreateExpense";
 import HelpPage from "./../components/HelpPage/HelpPage";
 import NotFound from "./../components/NotFound/NotFound";
 import EditExpense from "../components/EditExpense/EditExpense";
 import LoginPage from "../components/LoginPage/LoginPage";
+import PrivateRoute from "./PrivateRoute";
 
 export const history = createHistory();
 
@@ -15,13 +15,12 @@ const AppRouter = () => {
   return (
     <Router history={history}>
       <Fragment>
-        <Header />
         <Switch>
           <Route exact path="/" component={LoginPage} />
-          <Route path="/dashboard" component={ExpenseDashboardPage} />
-          <Route path="/create" component={CreateExpense} />
+          <PrivateRoute path="/dashboard" component={ExpenseDashboardPage} />
+          <PrivateRoute path="/create" component={CreateExpense} />
+          <PrivateRoute path="/edit/:id" component={EditExpense} />
           <Route path="/help" component={HelpPage} />
-          <Route path="/edit/:id" component={EditExpense} />
           <Route component={NotFound} />
         </Switch>
       </Fragment>
