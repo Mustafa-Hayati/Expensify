d98df9fa900943935d72ad912eab6fd87060aee9
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2020-10-07 13:13:13 +0330

    Create the startLogin middleware to logout the user. Add the logout button to the Header component.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,10 @@
 import "./Header.scss";
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { connect } from "react-redux";
+import { startLogout } from "../../actions/authActions";
 
-const Header = () => {
+const Header = ({ startLogout }) => {
   return (
     <header>
       <h1>Expensify</h1>
@@ -12,8 +14,15 @@ const Header = () => {
       <NavLink to="/create" activeClassName="header__active-link">
         CreateExpense
       </NavLink>
+      <button onClick={startLogout}>Logout</button>
     </header>
   );
 };
 
-export default Header;
+const mapDispatchToProps = dispatch => {
+  return {
+    startLogout: () => dispatch(startLogout()),
+  };
+};
+
+export default connect(undefined, mapDispatchToProps)(Header);
